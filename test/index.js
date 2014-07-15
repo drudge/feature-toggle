a104cf89b8f54fe71e3b121a7542c927d3335802
Sérgio Ramos <mail@sergioramos.me>
2014-07-15 01:04:32 +0100

    log events in test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,13 @@
 var assert = require('assert');
 var ft = require('../')(['not-active']);
 
+ft.on('activated', function(){
+  console.log('activated')
+});
+
+ft.on('deactivated', function(){
+  console.log('deactivated')
+});
 
 assert(ft.active('not-active') === true);
 ft.deactivate('not-active');
